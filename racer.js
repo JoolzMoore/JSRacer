fa556c13d55da877b69cfd389d12676e963bb05a
JoolzMoore <joolz.moore@gmail.com>
2016-05-06 14:12:27 +1200

    snail moves

--- racer.js
+++ racer.js
@@ -1,17 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
   console.log('hello');
   createEventListener();
-}
-)
+})
 
 function createEventListener() {
   document.addEventListener('keyup', function(e) {
-      //if p or q key pressed
-      if (e.which === 81) {
-        updatePlayer('player1_strip');
-      } else if (e.which === 80) {
-        updatePlayer('player2_strip');
-      }
+    //if p or q key pressed
+    if (e.which === 81) {
+      updatePlayer('player1_strip');
+    } else if (e.which === 80) {
+      updatePlayer('player2_strip');
+    }
   })
 }
 
@@ -19,37 +18,25 @@ function createEventListener() {
 function updatePlayer(player) {
   //navigate/ traverse to player strip 1 or 2
   var strip = document.getElementById(player);
-//define cell
+  //define cell
 
 
-  //set is found = false
-  var isFound = false;
   //loop through children in row
   for (var i = 0; i < strip.children.length; i++) {
-
-     var cell = strip.children [i];
-    //if condition is = active
-
-    //condition to know it is finished the game on teh last square
-
-
-    //call the restart function if teh statement is true
-
-    if (cell.className === "active") {
-      //set is not active and is found is true
-      cell className = "";
-      //set it to true
-      isfound = true;
+    //call the restart function if the statement is true
+    if (strip.children[i].className === "active") {
+      //if cell is active we need to check if end of row
+      if (i === strip.children.length -1) {
+        console.log ("you won")
+        return
       }
-      //if the cell is not active and is found it is true
-      else if (cell.className === "" && isFound) {
-      //then make it active
-
-      //reset as found
-    }
-
-
-
+      //change class name to nothing
+      strip.children[i].className = ""
+        //changng class of the cell to active
+      strip.children[i + 1].className = "active"
+      break
     }
 
   }
+
+}
